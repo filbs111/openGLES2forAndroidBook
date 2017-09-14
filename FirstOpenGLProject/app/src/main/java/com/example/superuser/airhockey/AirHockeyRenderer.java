8f49c29f86ed7599ed2560ee403f2f3e31405350
filbs111 <ed.filby@gmail.com>
2017-09-14 04:44:07 +0100

    chap2 - define verts and shaders

--- FirstOpenGLProject/app/src/main/java/com/example/superuser/airhockey/AirHockeyRenderer.java
+++ FirstOpenGLProject/app/src/main/java/com/example/superuser/airhockey/AirHockeyRenderer.java
@@ -5,14 +5,49 @@ import static android.opengl.GLES20.*;
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
-
 import android.opengl.GLSurfaceView.Renderer;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
 
 /**
  * Created by SuperUser on 14/09/2017.
  */
 
 public class AirHockeyRenderer implements Renderer {
+    private static final int POSITION_COMPONENT_COUNT = 2;
+    private static final int BYTES_PER_FLOAT = 4;
+    private final FloatBuffer vertexData;
+
+    public AirHockeyRenderer(){
+        float[] tableVertices = {
+                // Triangle 1
+                0f, 0f,
+                9f, 14f,
+                0f, 14f,
+
+                // Triangle 2
+                0f, 0f,
+                9f, 0f,
+                9f, 14f,
+
+                // Line 1
+                0f, 7f,
+                9f, 7f,
+
+                // Mallets
+                4.5f, 2f,
+                4.5f, 12f
+        };
+
+        vertexData = ByteBuffer
+                .allocateDirect(tableVertices.length * BYTES_PER_FLOAT)
+                .order(ByteOrder.nativeOrder())
+                .asFloatBuffer();
+        vertexData.put(tableVertices);
+    }
+
     @Override
     public void onSurfaceCreated(GL10 glUnused, EGLConfig config){
         glClearColor(1.0f, 0.0f, 0.0f, 0.0f);
